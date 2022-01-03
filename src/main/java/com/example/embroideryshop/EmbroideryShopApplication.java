267c6df125990e7ea38374f838b70274726b343b
Norbert Wojtowicz <norbi2790@gmail.com>
2022-01-03 06:07:39 +0100

    First commit

---
+++ src/main/java/com/example/embroideryshop/EmbroideryShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.embroideryshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbroideryShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbroideryShopApplication.class, args);
+    }
+
+}
