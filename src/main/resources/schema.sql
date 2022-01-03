dc8551f1f7fa56f3cfa75e2dc64916ea28eb03ce
Norbert Wojtowicz <norbi2790@gmail.com>
2022-01-03 15:48:15 +0100

    Changed Product column from title to name

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE PRODUCT (
-    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
-    title       VARCHAR(100)  NOT NULL,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100)  NOT NULL,
     description VARCHAR(2000) NULL,
-    price       DECIMAL(4, 2) NOT NULL
+    price DECIMAL(7, 2) NOT NULL
 );
