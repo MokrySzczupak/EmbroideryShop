4e291849420d2ec498078639f1b2734830dc96b7
Norbert Wojtowicz <norbi2790@gmail.com>
2022-03-15 19:34:26 +0100

    Add proper configuration for deployment

--- src/main/java/com/example/embroideryshop/EmbroideryShopApplication.java
+++ src/main/java/com/example/embroideryshop/EmbroideryShopApplication.java
@@ -2,11 +2,18 @@ package com.example.embroideryshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class EmbroideryShopApplication {
+public class EmbroideryShopApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EmbroideryShopApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EmbroideryShopApplication.class, args);
